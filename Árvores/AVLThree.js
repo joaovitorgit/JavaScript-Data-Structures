71f5dbfa9715d3728a89fbd9dc111257765dec03
Fulimirani <fulimirani@gmail.com>
2023-02-11 21:40:02 -0300

    11-02

--- Árvores/AVLThree.js
+++ Árvores/AVLThree.js
@@ -23,7 +23,9 @@ class Node{
 const BalanceFactor = {
     UNBALANCED_RIGTH: 1,    
     SLIGHTLY_UNBALANCED_RIGHT: 2,
-    
+    BALANCED: 3,
+    SLIGHTLY_UNBALANCED_LEFT:4,
+    UNBALANCED_LEFT: 5
 }
 
 class AVLThree extends BinarySearchTree{
@@ -46,7 +48,15 @@ class AVLThree extends BinarySearchTree{
         const heightDifference = this.getNodeHeight(node.left) - this.getNodeHeight(this.right)
         switch(heightDifference){
             case -2:
-                return this.getBalanceFactor
+                return BalanceFactor.UNBALANCED_RIGTH
+            case -1:
+                return BalanceFactor.SLIGHTLY_UNBALANCED_RIGHT
+            case 1:
+                return BalanceFactor.SLIGHTLY_UNBALANCED_LEFT
+            case 2:
+                return BalanceFactor.UNBALANCED_LEFT
+            default:
+                return BalanceFactor.BALANCED
         }
     }
 }
